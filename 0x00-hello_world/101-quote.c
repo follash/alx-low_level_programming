37b5774aaed35a2aaba983278df27c5355c7503c
follash <folawoleashiru@gmail.com>
2022-09-08 18:16:12 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * followed by a new line, to standard error.
+ * main - prints to string
+ * Description: Prints "and that piece of art is useful.." - Dora Korpar, 2015-10-19",
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar,
-			2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
