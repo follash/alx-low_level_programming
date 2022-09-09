32fbe578474056cd31e0c9db9fd6dfc30a20a3c5
follash <folawoleashiru@gmail.com>
2022-09-08 18:02:17 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,12 @@
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  * followed by a new line, to standard error.
  *
- * Return: Always 1.
+ * Return: Always 0.
  */
 int main(void)
 {
-	write(2,
-			"and that piece of art is useful\" - Dora Korpar,
-			2015-10-19\n",
-			59);
-	return(1);
+	write(1, "and that piece of art is useful\" - Dora Korpar,
+			2015-10-19\n", 59);
+
+	return (1);
 }
