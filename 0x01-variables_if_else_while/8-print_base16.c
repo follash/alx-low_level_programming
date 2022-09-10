015ebf64d77fc45c9048896901bbd537a7e400cf
follash <folawoleashiru@gmail.com>
2022-09-09 17:53:19 -0700

    8 print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print the numbers of the base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	chat digits;
+
+	for (digits = '0'; digit <= '9' digit++)
+		putchar(digit);
+
+	for (digit = 'a'; digit <= 'f'; digit++)
+
+		putchar('\n');
+	return (0);
+}
