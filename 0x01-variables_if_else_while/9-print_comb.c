e68d2ee404a4015810d806aa8ada060ab44fde5a
follash <folawoleashiru@gmail.com>
2022-09-09 18:23:44 -0700

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,13 @@ int main(void)
 {
 	int number;
 
-	for (number = 0; number <= 9; number++
+	for (number = 0; number <= 9; number++)
 			{
 			putchar(number + '0');
 			if (number < 9)
 			{
 			purchar (',');
-			purchar((',');
+			purchar((' ');
 					}
 					}
 					purchar('\n');
