fe61b118b063bd221846caf1c6bc43ebeb0fed0e
follash <folawoleashiru@gmail.com>
2022-09-23 15:40:09 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,7 +8,7 @@
 char *rot13(char *s)
 {
 	int i;
-
+	
 	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 	char *ptr = s;
